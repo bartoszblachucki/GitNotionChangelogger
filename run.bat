7c37c7b1e5b19fd3a4d4bc7c5f7587d9c51693f6
Bullet <bartek.blachucki@gmail.com>
2023-08-21 15:29:26 +0200

    style: fix run.bat comments getting displayed in the console

--- run.bat
+++ run.bat
@@ -1,7 +1,7 @@
+@echo off
 rem author: Bartosz Błachucki, GameDevTube
 rem This script runs the script m1_changelogger.py script using python and passes all arguments to it
 rem Use it to quickly iterate during development or if you don't want to/can't use a built exe
-@echo off
 
 rem Change the path to your Python executable (if needed)
 set python_executable=python
